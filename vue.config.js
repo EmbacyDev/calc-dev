fb020950b5dc08d18f8ea1b110923e95c371d2aa
Igor Shavlovsky <shahmanul@34gmail.com>
2022-08-23 21:11:34 +0300

    Добавил дохуя всего

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/calc-embacy/' : '/',
   transpileDependencies: true,
   css: {
     loaderOptions: {
