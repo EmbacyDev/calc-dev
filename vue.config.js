a8c573eea269cd19ac5b5de35f919bd0f4be69ce
Igor Shavlovsky <shahmanul@34gmail.com>
2022-08-23 22:08:34 +0300

    Добавил деплой на github

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/calc-embacy/' : '/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/calc-dev/'
+    : '/',
   transpileDependencies: true,
   css: {
     loaderOptions: {
